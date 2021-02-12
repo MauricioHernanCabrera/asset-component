d59e578d7fa1d6194e70db00ca791064b9d7ae27
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2021-02-12 12:58:21 -0300

    base project

---
+++ index.js
@@ -0,0 +1,13 @@
+const AssetController = require("./lib/asset.controller");
+const AssetDTO = require("./lib/asset.dto");
+const AssetFilter = require("./lib/asset.filter");
+const AssetSchema = require("./lib/asset.schema");
+const AssetService = require("./lib/asset.service");
+
+module.exports = {
+  AssetController,
+  AssetDTO,
+  AssetFilter,
+  AssetSchema,
+  AssetService,
+};
